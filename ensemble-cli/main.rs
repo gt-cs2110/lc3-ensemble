3459968413647f974e70dc912793edce3fbac27d
Henry Bui <henry.h.bui05@gmail.com>
2024-03-19 14:54:49 -0400

    restructure cli to eventually support run/debug

--- ensemble-cli/main.rs
+++ ensemble-cli/main.rs
@@ -1,41 +1,57 @@
 use std::borrow::Cow;
 use std::ops::Range;
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 use std::process::ExitCode;
 
 use ariadne::{ColorGenerator, Label, Report, ReportKind, Source};
-use clap::Parser;
+use clap::{Parser, Subcommand};
 use lc3_ensemble::asm::assemble;
 use lc3_ensemble::err::ErrSpan;
 use lc3_ensemble::parse::parse_ast;
 
 #[derive(Parser)]
+#[command(version, about, long_about = None)]
 struct Args {
-    #[arg(short, long)]
-    input: PathBuf,
-    #[arg(short, long)]
-    output: Option<PathBuf>
+    #[command(subcommand)]
+    cmd: Command
+}
+#[derive(Subcommand)]
+enum Command {
+    Assemble {
+        input: PathBuf,
+        #[arg(short, long)]
+        output: Option<PathBuf>
+    },
+    Run {
+        input: PathBuf,
+    },
+    Debug {
+        input: PathBuf,
+    }
 }
 
 struct SourceMetadata<'fp> {
     name: Cow<'fp, str>,
     src: Source<String>
 }
-fn main() -> ExitCode {
-    let Args { input, output } = Args::parse();
 
-    let src = match std::fs::read_to_string(&input) {
-        Ok(s) => s,
-        Err(e) => {
-            Report::<Range<_>>::build(ReportKind::Error, (), 0)
-                .with_message(format!("{}: {e}", input.display()))
-                .finish()
-                .eprint(Source::from(""))
-                .unwrap();
-            return ExitCode::FAILURE;
-        }
+fn main() -> ExitCode {
+    let Args { cmd } = Args::parse();
+
+    let result = match cmd {
+        Command::Assemble { input, output } => cmd_assemble(&input, output.as_deref().unwrap_or(&input)),
+        Command::Run { input } => cmd_run(&input),
+        Command::Debug { input } => cmd_debug(&input),
     };
 
+    match result {
+        Ok(_)  => ExitCode::SUCCESS,
+        Err(e) => e,
+    }
+}
+
+fn cmd_assemble(input: &Path, output: &Path) -> Result<(), ExitCode> {
+    let src = handle_read(input, std::fs::read_to_string)?;
 
     let meta = SourceMetadata {
         name: input.to_string_lossy(),
@@ -47,7 +63,7 @@ fn main() -> ExitCode {
                 Ok(t) => t,
                 Err(e) => {
                     report_error(e, &meta).unwrap();
-                    return ExitCode::FAILURE;
+                    return Err(ExitCode::FAILURE);
                 }
             }
         }
@@ -56,10 +72,27 @@ fn main() -> ExitCode {
     let ast = handle!(parse_ast(&src));
     let obj = handle!(assemble(ast));
     
-    // assemble(ast)
-    ExitCode::SUCCESS
+    Ok(())
+}
+fn cmd_run(input: &Path) -> Result<(), ExitCode> {
+    Err(ExitCode::FAILURE)
+}
+fn cmd_debug(input: &Path) -> Result<(), ExitCode> {
+    Err(ExitCode::FAILURE)
 }
 
+fn handle_read<'p, T>(input: &'p Path, read: impl FnOnce(&'p Path) -> std::io::Result<T>) -> Result<T, ExitCode> {
+    read(input)
+        .map_err(|e| {
+            Report::<Range<_>>::build(ReportKind::Error, (), 0)
+                .with_message(format!("{}: {e}", input.display()))
+                .finish()
+                .eprint(Source::from(""))
+                .unwrap();
+            
+            ExitCode::FAILURE
+        })
+}
 fn report_error<E: lc3_ensemble::err::Error>(err: E, meta: &SourceMetadata) -> std::io::Result<()> {
     let mut colors = ColorGenerator::new();
 
